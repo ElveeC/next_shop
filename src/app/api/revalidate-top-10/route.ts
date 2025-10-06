b97125b5abac52cd7302fb449a32a23fe05bdd5d
Elvira <elveechernenko@yandex.ru>
2025-10-06 23:34:47 +0300

    feat: add TOP_10_REQUEST_TAG variable

--- src/app/api/revalidate-top-10/route.ts
+++ src/app/api/revalidate-top-10/route.ts
@@ -1,8 +1,9 @@
 import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
+import { TOP_10_REQUEST_TAG } from "@/const/const";
 
 export function GET() {
-  revalidateTag("getTop10Rackets");
+  revalidateTag(TOP_10_REQUEST_TAG);
 
   return NextResponse.json({ status: 200 });
 }
--- src/app/services/get-top-10-rackets.ts
+++ src/app/services/get-top-10-rackets.ts
@@ -1,10 +1,11 @@
 import RacketType from "@/types/racket-type";
 import ResponseType from "@/types/response-type";
+import { TOP_10_REQUEST_TAG } from "@/const/const";
 
 export const getTop10Rackets = async (): Promise<ResponseType<RacketType[]>> => {
   const result = await fetch(`http://localhost:4000/api/top-10`, {
     next: {
-      tags: ["getTop10Rackets"],
+      tags: [TOP_10_REQUEST_TAG],
     }
   });
 
